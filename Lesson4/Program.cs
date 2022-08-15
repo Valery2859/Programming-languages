29d1432dea27eaff9866c6479f3c0cd5694a8f80
Valery <dobrinyvr@gmail.com>
2022-08-15 11:41:21 +0300

    Семинар четвёртого урока

---
+++ Lesson4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Program
+{
+static int PowTwoNumber(int number)
+{
+    return number * number;
+}
+static string MultipleTwoNumber(int number)
+{
+    return number % 2 == 0 ? "Кратное" : "Не кратное";
+}
+public static void Main(string[] args)
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(MultipleTwoNumber(PowTwoNumber(number)));
+
+    Console.ReadLine();
+
+}
+}
