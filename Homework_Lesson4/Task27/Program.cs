9017585ae4a2ec9d7f2132742a01db747b291d9e
Valery <dobrinyvr@gmail.com>
2022-08-16 14:45:39 +0300

    Правка

--- Homework_Lesson4/Task27/Program.cs
+++ Homework_Lesson4/Task27/Program.cs
@@ -1,29 +1,33 @@
 ﻿// Написать программу, которая принимает на вход число
 // и выдаёт сумму цифр в числе.
 
-Console.WriteLine("Введите любое число ");
-int a = Convert.ToInt32( Console.ReadLine() );
-
-int b = a;
-int d = 0; // кол-во цифр в числе
-
-while (b > 0)
+class Program 
 {
-  d++;
-  b = (int)(b/10);
-}
-   if (d<2)
-{
-  Console.WriteLine("Сумма цифр в числе {0} -> {1}", a, a);    
-}
-else 
-{
-    int result = 0;
-    for(int i = 1; i <= d; i++)
+  static void Main()
+ {  
+    Console.WriteLine("Введите любое число ");
+    int a = Convert.ToInt32( Console.ReadLine() );
+    int b = a;
+    int d = 0; // кол-во цифр в числе
+    while (b > 0)
     {
-        int c = (int) Math.Pow(10,i);
-        result = result + (a % c /(c/10));
+        d++;
+        b = (int)(b/10);
     }
-Console.WriteLine("Сумма цифр в числе {0} -> {1}", a, result);
-Console.WriteLine(" ");
+    if (d<2)
+    {
+        Console.WriteLine("Сумма цифр в числе {0} -> {1}", a, a);    
+    }
+    else 
+    {
+        int result = 0;
+        for(int i = 1; i <= d; i++)
+        {
+            int c = (int) Math.Pow(10,i);
+            result = result + (a % c /(c/10));
+        }
+        Console.WriteLine("Сумма цифр в числе {0} -> {1}", a, result);
+        Console.WriteLine(" ");
+    }
+  }
 }
