7e41c8650aeb37675f54d850885fcecdecfe513e
Valery <dobrinyvr@gmail.com>
2022-08-16 15:03:45 +0300

    Задача 29

--- Homework_Lesson4/Task29/Program.cs
+++ Homework_Lesson4/Task29/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из N элементов
+// и выводит их на экран
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество элементов массива");
+        int size = Convert.ToInt32( Console.ReadLine() ); 
+        int[] array = new int[size];
+        Random myRandom = new Random(); 
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = myRandom.Next(0, size);
+            Console.Write("{0} ", array[i]);
+        } 
+        Console.WriteLine(" ");
+    }
+}
