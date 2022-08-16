ee275886db9d3f82ed931916fee442c14a1f988d
Valery <dobrinyvr@gmail.com>
2022-08-16 08:07:53 +0300

    Исправлены ошибки

--- Homework_Lesson4/Task25/Program.cs
+++ Homework_Lesson4/Task25/Program.cs
@@ -1,24 +1,19 @@
-﻿// Написать цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
-
-using System;
+﻿// Написать цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
  
 class Program {
- public static int Main() 
+  static void Main()
  {  
-  Console.Write("Введите число: ");   //число, которое нужно возвести в степень
-  int a = Convert.ToInt32(Console.ReadLine());
-  
+  Console.Write("Введите число  : ");   //число, которое нужно возвести в степень
+  int a = Convert.ToInt32(Console.ReadLine());  
   Console.Write("Введите степень: "); //степень числа
-  int b = Convert.ToInt32(Console.ReadLine());
-  
+  int b = Convert.ToInt32(Console.ReadLine());  
   int result=1;                       //число, возведенное в степень
   for(int i=0; i<b; i++) 
   {
    result *= a;
   }
   Console.WriteLine("{0}^{1} = {2}", a, b, result);
-  Console.ReadKey();
-  return 0;
- }
+  Console.Write(" ");
+  }
 }
