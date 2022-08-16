54c13cde9d469bdc2d5098febb1f6d688219ece2
Valery <dobrinyvr@gmail.com>
2022-08-16 14:26:34 +0300

    Задача 27

--- Homework_Lesson4/Task27/Program.cs
+++ Homework_Lesson4/Task27/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите любое число ");
+int a = Convert.ToInt32( Console.ReadLine() );
+
+int b = a;
+int d = 0; // кол-во цифр в числе
+
+while (b > 0)
+{
+  d++;
+  b = (int)(b/10);
+}
+   if (d<2)
+{
+  Console.WriteLine("Сумма цифр в числе {0} -> {1}", a, a);    
+}
+else 
+{
+    int result = 0;
+    for(int i = 1; i <= d; i++)
+    {
+        int c = (int) Math.Pow(10,i);
+        result = result + (a % c /(c/10));
+    }
+Console.WriteLine("Сумма цифр в числе {0} -> {1}", a, result);
+Console.WriteLine(" ");
+}
