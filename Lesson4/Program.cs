bd4993e96bb0149182a6f7164ffd8842cce19c5c
Valery <dobrinyvr@gmail.com>
2022-08-18 11:34:31 +0300

    Save

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -10,12 +10,20 @@ static string MultipleTwoNumber(int number)
 {
     return number % 2 == 0 ? "Кратное" : "Не кратное";
 }
+static void ReadNumber(int number)
+{
+    Console.WriteLine(number * number);
+}
 public static void Main(string[] args)
 {
+    Console.WriteLine("Введите число ");
+    ReadNumber(Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine("Введите ещё одно число ");
     int number = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine(MultipleTwoNumber(PowTwoNumber(number)));
 
     Console.ReadLine();
+
 
 }
 }
