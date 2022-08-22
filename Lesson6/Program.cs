41b0a4060433aea9f3142394655f97bca0ef8a9c
Valery <dobrinyvr@gmail.com>
2022-08-22 10:20:21 +0300

    Урок №6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число ");
+        ReadNumber(Convert.ToInt32(Console.ReadLine()));
+        Console.WriteLine("Введите ещё одно число ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(MultipleTwoNumber(PowTwoNumber(number)));
+
+        Console.ReadLine();
+
+
+    }
+}
