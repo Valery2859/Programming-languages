8944c0c93ed621c65a88c8d60cd775f300b8608f
Valery <dobrinyvr@gmail.com>
2022-08-22 15:02:57 +0300

    Исправление косяков

--- Homework_Lesson6/Task35/Program.cs
+++ Homework_Lesson6/Task35/Program.cs
@@ -13,13 +13,13 @@ class Program
          int max=99, min=10, result = 0;
          for (int i = 0; i < array.Length; i++)
             {
-                array[i] = myRandom.Next(0,100 + size);              
+                array[i] = myRandom.Next(0,0 + size);              
                 Console.Write("{0} ", array[i]);
                 if ( array[i] < max && array[i] > min) result += 1;
                                
             } 
             Console.WriteLine(" ");
-            Console.WriteLine("количество элементов массива междe {0} и {1} -> {2}", min, max, result);
+            Console.WriteLine("Количество элементов массива между {0} и {1} -> {2}", min, max, result);
             Console.WriteLine(" ");  
 
     }
