7ee394d543eb3201469c56583945b19330972fc3
Valery <dobrinyvr@gmail.com>
2022-08-22 16:07:20 +0300

    Задача 37

--- Homework_Lesson6/Task37/Program.cs
+++ Homework_Lesson6/Task37/Program.cs
@@ -9,28 +9,35 @@ class Program
         int size = Convert.ToInt32(Console.ReadLine());
         int[] array = new int[size];
         Random myRandom = new Random();
-         int size2 = 0;
+        int size2 = 0;
+        int a = size - 1;
         for (int i = 0; i < array.Length; i++)
         {
-            array[i] = myRandom.Next(0, 100 + size);
+            array[i] = myRandom.Next(0, 10 + size);
             Console.Write("{0} ", array[i]);
         }
         Console.WriteLine(" ");
         size2 = size / 2;
+        if (size%2 !=0) size2 += 1;
+        int b = 0;
         int[] array2 = new int[size2];
-        for (int i = 0; i < size2; i++ )
+        for (int i = 0; i < size2; i++)
         {
-            array2[i] = array[i]*array[size2-i];
-            Console.Write("{0}, {1} ",i, array2[i]);
+            b = a - i;
+            if (i != b)
+                array2[i] = array[i] * array[b];
+            else
+                array2[i] = array[i];
+            Console.Write("{0} ", array2[i]);
         }
         Console.WriteLine(" ");
 
-        
 
 
-        
 
-       // Console.Write(" --> {0} чётных", result);
-      //  Console.WriteLine(" ");
+
+
+        // Console.Write(" --> {0} чётных", result);
+        //  Console.WriteLine(" ");
     }
 }
