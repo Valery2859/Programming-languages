dee355effde0f72b3c8908bd5a3db49c5dcb0610
Valery <dobrinyvr@gmail.com>
2022-08-29 11:56:28 +0300

    добавлены задачки

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -1,9 +1,10 @@
 ﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
 //может ли существовать треугольник с сторонами такой длины.
 
-
+/*
 class Program
 {
+    
     static void Main()
     {
         Console.WriteLine("Введите сторону а: ");
@@ -21,5 +22,26 @@ class Program
         {
             Console.WriteLine("Треугольник не существует");
         }
-    }
+    }*/
+//НАПИСАТЬ ПРОГРАММУ КОТОРАЯ ВВОДИТ МАССИВ ИЗ N ЭЛЕМЕНТОВ, ЗАПОЛНЕННЫЙ НУЛЯМИ
+// И ЕДИНИЦАМИ В СЛУЧАЙНОМ ПОРЯДКЕ
+int ReadStringConvertInt(string word)
+{
+    return int.Parse(word);
 }
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+void GetConsoleArray(int[] array)
+{
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+int number = ReadStringConvertInt(Console.ReadLine());
+int[] array = GetBinaryArray(number);
+GetConsoleArray(array);
